2871857617db64a65e7b0bc09a2be6d593199c11
Rondell <charles.rondell@gmail.com>
2020-03-29 02:38:47 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+let submitForm = document.getElementById('submit-form')
+
+
+
+function showForm() {
+    submitForm.style.display = 'block';
+}
+
+function formSent() {
+    location.reload(); 
+}
